d5355b69d23110118f6f18dc455756cab9c9be48
Neil Woodward <drlctr@gmail.com>
2014-05-17 15:09:50 -0700

    Added validations and tests to the Song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,11 @@
 class Song < ActiveRecord::Base
+
+validates :song_title, presence:  true
+validates :track, 
+	:numericality => {:greater_than => 0, :only_integer => true}
+validates :duration,
+	:numericality => {:greater_than => 0, :only_integer => true}
+validates :path, presence:  true
+validates :filename, presence: true
+
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,61 @@
 require 'spec_helper'
 
 describe Song do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:test_good){Song.new(song_title: "Title", track: 1, duration: 1, path: "path", filename: "filename")}
+  let(:test_no_title){Song.new(track: 1, duration: 1, path: "path", filename: "filename")}
+  let(:test_neg_track){Song.new(song_title: "Title", track: -1, duration: 1, path: "path", filename: "filename")}
+  let(:test_neg_dur){Song.new(song_title: "Title", track: 1, duration: 0, path: "path", filename: "filename")}
+  let(:test_no_path){Song.new(song_title: "Title", track: 1, duration: 1, filename: "filename")}
+  let(:test_no_fname){Song.new(song_title: "Title", track: 1, duration: 1, path: "path")}
+  let(:test_float_track){Song.new(song_title: "Title", track: 1.5, duration: 1, path: "path", filename: "filename")}
+  let(:test_float_dur){Song.new(song_title: "Title", track: 1, duration: 1.5, path: "path", filename: "filename")}
+
+  context "Testing correct validation" do
+
+  	it "must correctly validate a good record" do
+  	expect(test_good.valid?).to be_true
+    end  
+  end
+
+  context "testing song title validation" do
+
+  	it "must have a song title" do
+  		expect(test_no_title.valid?).to be_false
+  	end
+  end
+
+  context "Testing track number validation" do
+  	
+  	it "must not have a negative track value" do
+  	  expect(test_neg_track.valid?).to be_false
+    end
+
+    it "must not have a non-integer track number" do
+    	expect(test_float_track.valid?).to be_false
+    end
+  end
+
+  context "Testing duration validation" do
+
+  	it "must not have a negative duration" do
+  		expect(test_neg_dur.valid?).to be_false
+  	end
+
+  	it "must not have a non-integer duration" do
+  		expect(test_float_dur.valid?).to be_false
+  	end
+  end
+
+  context "Testing path and filename info" do
+
+  	it "must have a path" do
+  		expect(test_no_path.valid?).to be_false
+  	end
+
+  	it "must have a filename" do
+  		expect(test_no_fname.valid?).to be_false
+  	end
+  end
+
 end
