957669f7540765baa8a7b0ac549103c42f2f15c0
Neil Woodward <drlctr@gmail.com>
2014-05-17 23:46:50 -0700

    Added association between album and artist

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,8 @@
 class Album < ActiveRecord::Base
+	belongs_to :artist
+	has_many :songs
 
-validates :album_title, presence: true
-validates :genre, presence: true
+  validates :album_title, presence: true
+  validates :genre, presence: true
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140517234339) do
+ActiveRecord::Schema.define(version: 20140518031826) do
 
   create_table "albums", force: true do |t|
     t.string   "album_title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140517234339) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "protected"
+    t.integer  "artist_id"
   end
 
   create_table "artists", force: true do |t|
