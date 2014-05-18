9ca28844184e0834324d4738be2abcb1542d5c8f
Neil Woodward <drlctr@gmail.com>
2014-05-18 11:31:20 -0700

    Added rspec tests to session model.

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,4 +2,11 @@ class Session < ActiveRecord::Base
 	belongs_to :song
 	belongs_to :musician
 
+	validates :song_id, presence: true
+	validates :song_id, 
+		:numericality => {:greater_than => 0, :only_integer => true}
+	validates :musician_id, presence: true
+	validates :musician_id, 
+		:numericality => {:greater_than => 0, :only_integer => true}
+
 end
--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe Session do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:test_good){Session.new(song_id: 1, musician_id:1)}
+  let(:test_bad_song_id){Session.new(song_id: "A", musician_id:1)}
+  let(:test_missing_song_id){Session.new(musician_id:1)}
+  let(:test_bad_musician_id){Session.new(song_id: 1, musician_id:"B")}
+  let(:test_missing_musician_id){Session.new(song_id: 1)}
+
+  context "When validating the session, it" do
+
+  	it "should validate a correct record" do
+  		expect(test_good.valid?).to be_true
+  	end
+
+  	it "should ensure that the song_id is an integer" do
+  	  expect(test_bad_song_id.valid?).to be_false
+  	end
+
+  	it "should ensure that there is a song_id" do
+  		expect(test_missing_song_id.valid?).to be_false
+  	end
+
+  	it "should ensure that the musician_id is an integer" do
+  	  expect(test_bad_musician_id.valid?).to be_false
+  	end
+
+  	it "should ensure that there is a musician_id" do
+  		expect(test_missing_musician_id.valid?).to be_false
+  	end
+  end	
 end
