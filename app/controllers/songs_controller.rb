a5e366ab88933c8bef0905b42682da9f0d73f9cd
Neil Woodward <drlctr@gmail.com>
2014-05-24 23:01:59 -0700

    Now includes new song that adds a new artist and/or album.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,7 +28,46 @@ class SongsController < ApplicationController
   # POST /songs
   # POST /songs.json
   def create
+
+    if Artist.all.collect{|a| a.name}.include? params[:artist_name] 
+      a = Artist.where(name: params[:artist_name])
+      artist_id = a[0].id
+      puts "Yep.  The Artist is here.  artist_id = #{artist_id}"
+    else
+      @artist=Artist.new(name: params[:artist_name])
+      if @artist.name.empty?
+        redirect_to action: :blank_artist and return
+      elsif !@artist.save
+        format.html { render action: 'new' }
+        format.json { render json: @artist.errors, status: :unprocessable_entity }
+      end
+      artist_id = @artist.id
+      puts "Nope.  Artist wasn't there.  New artist_id = #{artist_id}"
+    end
+
+    if Album.all.collect{|a| a.album_title}.include? params[:album_name] 
+
+      a = Album.where(album_title: params[:album_name])
+      album_id = a[0].id
+      puts "Yep.  The Album is here.  album_id = #{album_id}, artist_id = #{artist_id}"
+    else
+      @album=Album.new(album_title: params[:album_name], genre: params[:genre], artist_id: artist_id)
+      puts "creating a new album with album title:  #{params[:album_name]},
+        genre:  #{params[:genre]}, artist_id: #{artist_id}"
+      if @album.album_title.empty?||@album.genre.empty?
+        redirect_to action: :blank_album_data and return
+      elsif !@album.save
+        puts "In the failed album save branch."
+        format.html { render action: 'new' }
+        format.json { render json: @album.errors, status: :unprocessable_entity }
+      end
+      album_id = @album.id
+      puts "Nope.  Album wasn't there.  New album_id = #{album_id}"
+    end
+
+
     @song = Song.new(song_params)
+    @song.album_id = album_id
 
     respond_to do |format|
       if @song.save
@@ -75,5 +114,8 @@ class SongsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def song_params
       params.require(:song).permit(:song_title, :track, :duration, :path, :filename, :composer)
+    end
+
+    def blank_album_data
     end
 end
