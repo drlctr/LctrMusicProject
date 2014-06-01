d01cabf5b267dc064341bf3984b92cf9f35e23f6
Neil Woodward <drlctr@gmail.com>
2014-05-31 23:36:09 -0700

    Added the FactoryGirl factories.  Fixed all artists_controller fails; now clean in rspec.

---
+++ spec/factories/factories.rb
@@ -0,0 +1,43 @@
+FactoryGirl.define do 
+
+	factory :instrument do
+		name	'guitar'
+	end
+
+	factory :musician do
+		instrument
+		first_name 	'John'
+		last_name		'Smith'
+		born				'1962'
+	end
+
+	factory :artist do
+		name        'Testers'
+		nationality	'USA'
+	end
+
+	factory :album do
+		artist
+		id          '1'
+		album_title	'Test_album'
+		year        '1988'
+		genre       'Test_genre'
+		protected   'false'
+	end
+
+	factory :song do
+		album
+		song_title  'Test_song'
+		track       '1/10'
+		duration    '180'
+		path        '/test/path'
+		filename    'test_file.mp3'
+		composer    'test_composer'
+	end
+
+	factory :user do
+		email       'test@test.com'
+		password    '123456'
+	end
+
+end
